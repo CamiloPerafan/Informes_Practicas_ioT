45cbebc8e731e0ce162a2d2d65c28dc0cce8967f
cperafane <cperafane@unicauca.edu.co>
2022-11-29 00:30:02 -0500

    Se completo la tarea

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,64 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface Auto {
+
+  marca: string,
+  modelo: string,
+  capacidad: number
+}
 
 @Controller()
 export class AppController {
-   constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-   private persona = "Mundo";
+  private Autos : Auto[] = [{
+    marca: "BWM",
+    modelo: "M4",
+    capacidad: 2
+  }]
 
-   @Get()
-   getHello(): string {
-      return `Hola: ${this.persona}`
-   }
+  @Get()
+  getHello(): Auto[] {
+    return this.Autos;
+  }
 
-   @Post(':nombre')
-   modificar(@Param('nombre') nombre: string): string {
-      this.persona = nombre;
-      return `Mensaje modificado: ${this.persona}`
-   }
+  @Post()
+  crear(@Body() datos: Auto): Auto {
+    this.Autos.push(datos);
+    return datos;
+  }
+
+  @Put(":id")
+  modificar(@Body() datos: Auto, @Param('id') id: number): Auto | string {
+    try{
+    this.Autos[id] = datos
+    return this.Autos[id];
+    }
+    catch{
+      return `No fue posible modificar el automovil en la posición ${id}`
+    }
+  }
+
+  @Delete(":id")
+  eliminar(@Param('id') id: number){
+    try{
+      this.Autos = this.Autos.filter((val, index) => index != id);
+      return true;
+    }
+    catch{
+      return false;
+    }
+  }
+
+  @Patch(":id/capacidad/:capacidad")
+  cambiarcapacidad(@Param('id') id: number, @Param('capacidad') capacidad: number): Auto | string{
+    try{
+      this.Autos[id].capacidad = capacidad;
+      return this.Autos[id];
+    }
+    catch{
+      return `No fue posible modificar el automovil en la posición ${id}`
+    }
+  }
 }
